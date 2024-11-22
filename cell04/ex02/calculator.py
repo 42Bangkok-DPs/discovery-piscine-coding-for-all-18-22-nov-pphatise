5181c5eacddb0dd7858ab93297f537ea9486ac3c
pphatise <67030317@kmitl.ac.th>
2024-11-22 09:11:36 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input("Give me the first number:"))
+num2 = int(input("Give me the second number:"))
+print("Thank you!")
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+print(f"{num1} + {num2} = {add}")
+print(f"{num1} - {num2} = {subtract}")
+print(f"{num1} * {num2} = {multiply}")
+print(f"{num1} / {num2} = {divide}")
